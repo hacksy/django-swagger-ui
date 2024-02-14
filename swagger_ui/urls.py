67ccca064e52756766d3838bd4e23e2ecf3cae79
hansyschmitt <55103581+hansyschmitt@users.noreply.github.com>
2024-02-13 23:46:27 -0500

    Add static files and update url

--- swagger_ui/urls.py
+++ swagger_ui/urls.py
@@ -6,11 +6,11 @@ try:
     from django.urls import path
 
     urlpatterns = [
-        path('api-doc/', yaml_to_html, name="api-doc"),
+        path('docs/', yaml_to_html, name="api-doc"),
     ]
 except:
     from django.conf.urls import  url
 
     urlpatterns = [
-        url(r'^api-doc/', yaml_to_html, name="api-doc"),
+        url(r'^docs/', yaml_to_html, name="api-doc"),
     ]
